ea49ca2aeba325bac276653e11ff569a021f89f9
yst520 <1723611230@qq.com>
2022-03-09 10:08:39 +0800

    后台店主功能基本完成

--- nodejs/router/goods.js
+++ nodejs/router/goods.js
@@ -4,7 +4,7 @@ const boom = require('boom')
 const Result = require('../model/Result')
 const router = express.Router()
 // const { insert } = require('../db/index')
-const {getGoodsList,insertScore,updateScore,isNew,deleteScore} = require('../service/goods')
+const {getGoodsList,getShenheList,getXiajiaList,insertGoods,sendAdmin,updateGoods,updateShenhe,isNew,deleteGoods,deleteShenhe} = require('../service/goods')
 
 router.get('/goodsList',(req,res,next)=>{
   getGoodsList(req.query).then(({list,count,page,pageSize})=>{
@@ -13,32 +13,78 @@ router.get('/goodsList',(req,res,next)=>{
     next(boom.badImplementation(err))
   })
 })
-
-router.post('/insertScore',(req,res,next)=>{
+//审核数据
+router.get('/shenheList',(req,res,next)=>{
+  getShenheList(req.query).then(({list,count,page,pageSize})=>{
+    new Result({list,count,page:+page,pageSize:+pageSize},'获取审核数据成功').success(res)
+  }).catch(err=>{ 
+    next(boom.badImplementation(err))
+  })
+})
+//审核数据
+router.get('/xiajiaList',(req,res,next)=>{
+  getXiajiaList(req.query).then(({list,count,page,pageSize})=>{
+    new Result({list,count,page:+page,pageSize:+pageSize},'获取审核数据成功').success(res)
+  }).catch(err=>{ 
+    next(boom.badImplementation(err))
+  })
+})
+router.post('/insertGoods',(req,res,next)=>{
   // console.log(req.body);
   // new Result().success(res)
-  insertScore(req.body).then(()=>{
+  insertGoods(req.body).then(()=>{
     new Result('添加成功').success(res)
   }).catch(err=>{
     // next(boom.badImplementation(err))
     console.log({err});
-    new Result('已经有此记录').success(res)
+    new Result('已经有此记录了，请选择编辑商品信息').success(res)
   })
 })
-router.post('/updateScore',(req,res,next)=>{
+//送审管理员
+router.post('/sendAdmin',(req,res,next)=>{
   // console.log(req.body);
   // new Result().success(res)
-  updateScore(req.body).then(()=>{
+  sendAdmin(req.body).then(()=>{
+    new Result('数据已经送至管理员审核，请耐心等待').success(res)
+  }).catch(err=>{
+    // next(boom.badImplementation(err))
+    console.log({err});
+    new Result('已经有此记录了，请选择编辑商品信息').success(res)
+  })
+})
+router.post('/updateGoods',(req,res,next)=>{
+  // console.log(req.body);
+  // new Result().success(res)
+  updateGoods(req.body).then(()=>{
+    new Result('修改成功').success(res)
+  }).catch(err=>{
+    next(boom.badImplementation(err))
+  })
+})
+router.post('/updateShenhe',(req,res,next)=>{
+  // console.log(req.body);
+  // new Result().success(res)
+  updateShenhe(req.body).then(()=>{
     new Result('修改成功').success(res)
   }).catch(err=>{
     next(boom.badImplementation(err))
   })
 })
 
-router.post('/deleteScore',(req,res,next)=>{
+router.post('/deleteGoods',(req,res,next)=>{
   console.log("req",req);
   // const {sno,cno}=req.body
-  deleteScore(req.body).then((result)=>{
+  deleteGoods(req.body).then((result)=>{
+    new Result('删除成功').success(res)
+  }).catch((err)=>{
+    new Result('删除失败').fail(res)
+  })
+})
+//删除审核数据
+router.post('/deleteShenhe',(req,res,next)=>{
+  console.log("req",req);
+  // const {id}=req.body
+  deleteShenhe(req.body).then((result)=>{
     new Result('删除成功').success(res)
   }).catch((err)=>{
     new Result('删除失败').fail(res)
--- nodejs/service/goods.js
+++ nodejs/service/goods.js
@@ -44,26 +44,124 @@ async function getGoodsList(query) {
 
   return { list, count: count[0].count, page, pageSize }
 }
+async function getShenheList(query) {
+  // console.log(query);
+  const {
+    // 默认值page=1,pageSize=20
+    id, name, page = 1, pageSize = 4, sort
+  } = query
+  // 偏移多少个数据量，page=0 offset=0查询第1个  
+  // page=1 offset=20 查询第21个
+  const offset = (page - 1) * pageSize
+  let scoreSql = `select * from shenhe where state='审核中'`
+  let where = 'where'
 
+  // where  第二个参数 ：key  第三个：value
+  id && (where = db.and(where, "id", id))
+  // 模糊查询
+  name && (where = db.andLike(where, 'name',name))
+  if (where !== 'where') {
+    scoreSql = `${scoreSql} ${where}`
+  }
+  if (sort) {
+    // symbol:第一个字符
+    const symbol = sort[0]
+    const colunm = sort.slice(1, sort.length)
+    const order = symbol === '+' ? 'asc' : 'desc'
+    scoreSql = `${scoreSql} order by \`${colunm}\` ${order}`
+  }
+  // 统计一共多少审核数据
+  let countSql = `select count(*) as count from shenhe where state='审核中'`
+  // 有查询条件
+  if (where != 'where') {
+    countSql = `${countSql} ${where}`
+  }
+  const count = await db.querySql(countSql)
+  console.log("count", count);
+  scoreSql = `${scoreSql} limit ${pageSize} offset ${offset}`
+  const list = await db.querySql(scoreSql)
+
+
+  return { list, count: count[0].count, page, pageSize }
+}
+async function getXiajiaList(query) {
+  // console.log(query);
+  const {
+    // 默认值page=1,pageSize=20
+    id, name, page = 1, pageSize = 10, sort
+  } = query
+  // 偏移多少个数据量，page=0 offset=0查询第1个  
+  // page=1 offset=20 查询第21个
+  const offset = (page - 1) * pageSize
+  let scoreSql = `select * from goodslist where state='已下架'`
+  let where = 'where'
+
+  // where  第二个参数 ：key  第三个：value
+  id && (where = db.and(where, "id", id))
+  // 模糊查询
+  name && (where = db.andLike(where, 'name',name))
+  if (where !== 'where') {
+    scoreSql = `${scoreSql} ${where}`
+  }
+  if (sort) {
+    // symbol:第一个字符
+    const symbol = sort[0]
+    const colunm = sort.slice(1, sort.length)
+    const order = symbol === '+' ? 'asc' : 'desc'
+    scoreSql = `${scoreSql} order by \`${colunm}\` ${order}`
+  }
+  // 统计一共多少审核数据
+  let countSql = `select count(*) as count from goodslist where state='已下架'`
+  // 有查询条件
+  if (where != 'where') {
+    countSql = `${countSql} ${where}`
+  }
+  const count = await db.querySql(countSql)
+  console.log("count", count);
+  scoreSql = `${scoreSql} limit ${pageSize} offset ${offset}`
+  const list = await db.querySql(scoreSql)
+
+
+  return { list, count: count[0].count, page, pageSize }
+}
 function exists(body) {
   // return false
-  const { sno, cno, grade } = body
-  const sql = `select * from goodslist where sno='${sno}' and cno='${cno}'`
+  const { id } = body
+  const sql = `select * from goodslist where id='${id}'`
   return db.queryOne(sql)
 }
 
-function insertScore(body) {
+function sendAdmin(body) {
   return new Promise(async (resolve, reject) => {
     try {
       // 判断电子书是否已经上传过了
       const result = await exists(body)
       // console.log('res', result);
       if (result) {
-        reject(new Error('已经有此记录了，请选择编辑分数'))
+        reject(new Error('已经有此记录了，请选择编辑商品信息'))
 
       } else {
         // 传入对象和表名 完成插入操作
-        await db.insert(body, 'sc')
+        await db.insert(body, 'shenhe')
+        resolve()
+      }
+    } catch (error) {
+      reject(error)
+    }
+  })
+}
+function insertGoods(body) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      // 判断电子书是否已经上传过了
+      const result = await exists(body)
+      // console.log('res', result);
+      if (result) {
+        reject(new Error('已经有此记录了，请选择编辑商品信息'))
+
+      } else {
+        // 传入对象和表名 完成插入操作
+        await db.insert(body, 'goodslist')
         resolve()
       }
     } catch (error) {
@@ -72,10 +170,20 @@ function insertScore(body) {
   })
 }
 
-function updateScore(body) {
+function updateGoods(body) {
   return new Promise(async (resolve, reject) => {
     try {
-      await db.update(body, 'sc', `where sno=${body.sno} and cno=${body.cno}`)
+      await db.update(body, 'goodslist', `where id=${body.id}`)
+      resolve()
+    } catch (error) {
+      reject("error", error)
+    }
+  })
+}
+function updateShenhe(body) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      await db.update(body, 'shenhe', `where id=${body.id}`)
       resolve()
     } catch (error) {
       reject("error", error)
@@ -83,10 +191,21 @@ function updateScore(body) {
   })
 }
 
-function deleteScore(body) {
-  const { sno, cno } = body
+function deleteGoods(body) {
+  const { id} = body
   return new Promise(async (resolve, reject) => {
-    await db.queryOne(`delete from sc where sno=${sno} and cno=${cno}`).then((res) => {
+    await db.queryOne(`delete from goodslist where id=${id}`).then((res) => {
+      resolve('删除成功')
+    }).catch((err) => {
+      reject(new Error('删除失败'))
+    })
+  })
+
+}
+function deleteShenhe(body) {
+  const { id} = body
+  return new Promise(async (resolve, reject) => {
+    await db.queryOne(`delete from shenhe where id=${id}`).then((res) => {
       resolve('删除成功')
     }).catch((err) => {
       reject(new Error('删除失败'))
@@ -98,25 +217,25 @@ function isNew(params) {
   return new Promise(async (resolve, reject) => {
 
     console.log("params111", params);
-    const { cno, sno } = params
+    const { id, name } = params
 
     let result = []
     // 如果username输入了值的话
-    if (cno) {
-      if (isNumber(cno) == false) {
+    if (id) {
+      if (isNumber(id) == false) {
         reject('请输入数字')
       }
-      result = await db.queryOne(`select * from course where cno='${cno}'`)
+      result = await db.queryOne(`select * from goodslist where id='${id}'`)
       if (!result || result == []) {
         // console.log("result: ",result.length);
         reject('无该课程')
       }
     }
-    if (sno) {
-      if (isNumber(sno) == false) {
+    if (name) {
+      if (isNumber(name) == false) {
         reject('请输入数字')
       }
-      result = await db.queryOne(`select * from stu where sno='${sno}'`)
+      result = await db.queryOne(`select * from goodslist where name='${name}'`)
       if (!result || result == []) {
         // console.log("result: ",result.length);
         reject('无该学生')
@@ -131,4 +250,4 @@ function isNew(params) {
     resolve()
   })
 }
-module.exports = { getGoodsList, insertScore, updateScore, isNew, deleteScore }
+module.exports = { getGoodsList,getShenheList,getXiajiaList, sendAdmin,insertGoods,deleteShenhe, updateGoods,updateShenhe, isNew, deleteGoods }
